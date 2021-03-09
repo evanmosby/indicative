fe960e6a73f286e2d7ac552e995d4b007ded9687
Andrew Derringer <aderringer@esri.com>
2021-03-08 16:14:03 -0800

    check existy is running

--- src/raw/existy.js
+++ src/raw/existy.js
@@ -1,5 +1,6 @@
 module.exports = (input) => {
   if (typeof input === "string") {
+    console.log("in existy.string");
     return input.trim().length >= 0;
   }
   return input !== null && input !== undefined;
